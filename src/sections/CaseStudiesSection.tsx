5b91a1d0b3373cd9d74b812503241d398e49d759
MHamzaAamir <hamza.aamir.rasheed2@gmail.com>
2026-07-01 00:40:52 +0500

    progress on client case studies

--- src/sections/CaseStudiesSection.tsx
+++ src/sections/CaseStudiesSection.tsx
@@ -14,15 +14,33 @@ const cards: ClientCardProps[] = [
     text: "This is test string3",
     image: "/hero-poster.jpg",
   },
+  {
+    text: "This is test string4",
+    image: "/hero-poster.jpg",
+  },
+  {
+    text: "This is test string5",
+    image: "/hero-poster.jpg",
+  },
+  {
+    text: "This is test string6",
+    image: "/hero-poster.jpg",
+  },
 ];
 
 export default function CaseStudiesSection() {
   return (
-    <section className="h-screen w-screen">
-      <div className="h-full bg-amber-950 flex items-center justify-center gap-20">
-        {cards.map((card) => (
-          <ClientCard key={card.text} text={card.text} image={card.image} />
-        ))}
+    <section className="w-screen bg-[#000123]">
+      <div className="section-container h-full flex flex-col py-20">
+        <div className="flex flex-col items-center">
+          <h1 className="text-7xl">OUR CLIENTS &</h1>
+          <h1 className="text-[79px] font-bold">CASE STUDIES</h1>
+        </div>
+        <div className="flex justify-center items-center flex-wrap gap-12 py-10">
+          {cards.map((card) => (
+            <ClientCard key={card.text} text={card.text} image={card.image} />
+          ))}
+        </div>
       </div>
     </section>
   );
